d5ee2b6326d9a64f2e1aae6845bb16da23631cc3
Rajendro1 <rajandroprosad@gmail.com>
2024-09-30 23:23:38 +0530

    code Reformat

--- tests/example_test.go
+++ tests/example_test.go
@@ -1,10 +1,11 @@
 package tests
 
 import (
-    "testing"
-    "github.com/stretchr/testify/assert"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestExample(t *testing.T) {
-    assert.Equal(t, 1, 1, "Expectation to match reality.")
+	assert.Equal(t, 1, 1, "Expectation to match reality.")
 }
